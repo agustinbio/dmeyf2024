419f47f561e9eb1c983495a145b3a0da2a0a80b8
agustinbio <agustinbio@inventati.org>
2024-11-23 19:23:11 +0000

    auto commit 2       20241123 192304

--- src/workflows/909_run_orden227.r
+++ src/workflows/909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/990_workflow_orden227_SEMI.r"
+PARAM <- "src/workflows/990_workflow_orden227_SEMI_deflacion_sin_abril2021.r"
 
 envg <- env()
 
